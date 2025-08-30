81e45cade8bfb3aa6bf3f3fb17d99d9c5f46cdca
Jada Rose <143419259+Jadarose01@users.noreply.github.com>
2025-08-29 21:04:42 -0400

    Update sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,19 @@
-// YOUR NAME HERE
+// JADA ROSE
 #include <iostream>
 using namespace std;
 
-int main () {
+int main() {
+    int num1 = 15;  // first integer
+    int num2 = 22;  // second integer
+    int sum;
 
+    // Calculate sum
+    sum = num1 + num2;
+
+    // Output result
+    cout << "First integer: " << num1 << endl;
+    cout << "Second integer: " << num2 << endl;
+    cout << "The sum is " << sum << "." << endl;
+
+    return 0;
 }
